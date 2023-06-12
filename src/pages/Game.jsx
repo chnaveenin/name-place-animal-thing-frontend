29a423b6e54419b561970b608b86cf447cecba5a
chnaveenin <chnaveen.in@gmail.com>
2023-06-12 22:08:04 +0530

    added join room

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -2,25 +2,41 @@ import React, {useState, useEffect} from "react"
 import { useParams } from "react-router"
 import { Typography } from "@mui/material"
 import CircularProgress from '@mui/material/CircularProgress';
+import { Details } from "@mui/icons-material";
 
 
 const Game = () => {
   const { roomid } = useParams()
   const [loading, setLoading] = useState(true)
-  const [isRoomExists, setIsRoomExists] = useState(true)
+  const [isRoomExists, setIsRoomExists] = useState(false)
+  const [roomDetails, setRoomDetails] = useState()
 
   useEffect(() => {
-    // check from backend if the room exists
-    setIsRoomExists(false)
+    const getRoomDetails = async () => {
+      const response = await fetch("http://localhost:8080/room/" + roomid, {
+        method: "GET"
+      })
+
+      if (response.ok) {
+        const data = await response.json()
+        console.log(data)
+        setIsRoomExists(true)
+        setRoomDetails(data)
+      } else {
+        setIsRoomExists(false)
+      }
+    }
+
+    getRoomDetails()
     setLoading(false)
-  }) 
+  }, [])
 
   return (
-    loading 
+    (loading 
       ? 
       <CircularProgress />
       :
-      isRoomExists
+      (isRoomExists
         ?
         <Typography variant="subtitle1">
           Welcome to the Room, Room-ID: {roomid}
@@ -29,6 +45,8 @@ const Game = () => {
         <Typography variant="subtitle1">
           No room found with room-id: {roomid}
         </Typography>
+      )
+    )
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,55 @@
+import React, { useState } from "react"
+import { useNavigate } from "react-router";
 import {
   Box,
   Button,
-  TextField
+  IconButton,
+  TextField,
+  Typography
 } from "@mui/material"
-import React, { useState } from "react"
-import { useNavigate } from "react-router";
+import DoneAllOutlinedIcon from '@mui/icons-material/DoneAllOutlined';
 
 const Home = () => {
+  const [username, setUsername] = useState("")
+  const [save, setSave] = useState(false)
   const [join, setJoin] = useState(false)
   const [roomId, setRoomId] = useState("")
+  const [error, setError] = useState("")
 
   const navigate = useNavigate()
 
-  const createRoom = () => {
+  const createRoom = async () => {
     console.log("creating room")
     setJoin(false)
-    navigate("/" + generateRandomString(6))
+    while (true) {
+      const temp = generateRandomString(6)
+      console.log(temp)
+      const response = await fetch("http://localhost:8080/room/" + temp, {
+        method: "GET"
+      })
+
+      if (!response.ok) {
+        setRoomId(temp)
+        console.log(roomId)
+        const response = await fetch("http://localhost:8080/create-room", {
+          method: "POST",
+          body: JSON.stringify({
+            roomid: temp,
+            participants: [{
+              name: username,
+              score: 0
+            }]
+          }),
+          headers: {
+            "Content-Type": "application/json"
+          }
+        })
+        if (response.ok) {
+          navigate("/" + temp)
+        }
+        break
+      }
+    }
   }
 
   const joinRoom = () => {
@@ -23,8 +57,24 @@ const Home = () => {
     setJoin(true)
   }
 
-  const enterRoom = () => {
-    navigate("/" + roomId)
+  const enterRoom = async () => {
+    const response = await fetch("http://localhost:8080/join-room/" + roomId, {
+      method: "PATCH",
+      body: JSON.stringify({
+          name: username,
+          score: 0
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+
+
+    if (response.ok) {
+      navigate("/" + roomId)
+    } else {
+      setError("no room found")
+    }
   }
 
   function generateRandomString(length) {
@@ -43,17 +93,42 @@ const Home = () => {
         <Box
           m={1}
           display="flex"
+          flexDirection="column"
           justifyContent="center"
           alignItems="center"
+          sx={{marginBottom: "15%"}}
         >
-          <Button
-            variant="contained"
-            color="secondary"
-            sx={{ height: 40, width: 150 }}
-            onClick={createRoom}
+          <Typography
+            variant="h2"
+            color="linen"
+            sx={{marginBottom: "0.5em"}}
           >
-            Create Room
-          </Button>
+            Welcome {username}
+          </Typography>
+          {!save &&
+            <form
+              onSubmit={(e) => {
+                e.preventDefault()
+                setSave(true)
+              }}
+            >
+              <TextField
+                id="outlined-basic"
+                label="username"
+                variant="filled"
+                autoComplete="off"
+                color="warning"
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              <IconButton
+                color="white"
+                aria-label="save username"
+                onClick={() => setSave(true)}
+              >
+                <DoneAllOutlinedIcon />
+              </IconButton>
+            </form>
+          }
         </Box>
         <Box
           m={1}
@@ -61,6 +136,14 @@ const Home = () => {
           justifyContent="center"
           alignItems="center"
         >
+          <Button
+            variant="contained"
+            color="secondary"
+            sx={{ height: 40, width: 150, marginRight: "1em" }}
+            onClick={createRoom}
+          >
+            Create Room
+          </Button>
           <Button
             variant="contained"
             color="success"
@@ -77,21 +160,30 @@ const Home = () => {
                 justifyContent="center"
                 alignItems="center"
               >
-                <TextField
-                  id="outlined-basic"
-                  label="room-id"
-                  variant="outlined"
-                  autocomplete="off"
-                  onChange={(e) => setRoomId(e.target.value)}
-                />
-                <Button
-                  variant="contained"
-                  color="primary"
-                  sx={{ height: 40, marginLeft: "8px" }}
-                  onClick={enterRoom}
+                <form
+                  onSubmit={(e) => {
+                    e.preventDefault()
+                    enterRoom()
+                  }}
                 >
-                  join
-                </Button>
+                  <TextField
+                    id="outlined-basic"
+                    label="roomid"
+                    variant="filled"
+                    autoComplete="off"
+                    color="warning"
+                    onChange={(e) => setRoomId(e.target.value)}
+                  />
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={enterRoom}
+                    sx={{marginLeft: "10px", marginTop: "10px"}}
+                  >
+                    join
+                  </Button>
+                </form>
+                {error && <Typography sx={{marginLeft: "10px"}} color="red">{error}</Typography>}
               </Box>
             )
           }
